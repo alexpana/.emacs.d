323ed01313ef3dd7999cedf38f5f7030f16eecee
Alexandru Pana <alexandru.pana@manabreakstudios.com>
2017-12-14 12:36:26 +0200

    Add keybindings for duplicate line and helm-find-files

--- init-keybindings.el
+++ init-keybindings.el
@@ -1,3 +1,14 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-/") 'comment-line)
+
+
+(defun duplicate-line ()
+  "Duplicate current line"
+  (interactive)
+  (kill-whole-line)
+  (yank)
+  (yank)
+	(previous-line))
+(global-set-key (kbd "C-d") 'duplicate-line)
