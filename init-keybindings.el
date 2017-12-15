b09289b2ced3a2b6f69aaf65cc8a86005e95c225
Alexandru Pana <alexandru.pana@manabreakstudios.com>
2017-12-15 14:38:38 +0200

    Add helpful unreal snippets

--- init-keybindings.el
+++ init-keybindings.el
@@ -22,6 +22,22 @@
 	(save-excursion
 		(indent-region (point-min) (point-max) nil)))
 
+;; Searches for the class name by using the buffer name and other declarations
+;; present in the buffer. This helps detect class names using prefixes (such as
+;; unreal classes) that differ from the file name
+(defun cpp-class-name-in-buffer ()
+	(interactive)
+	(save-excursion
+		(isearch-forward nil 1)
+		(isearch-yank-string
+		 (file-name-nondirectory
+			(file-name-sans-extension (buffer-file-name))))
+		(isearch-exit)
+		(backward-sexp)
+		(push-mark)
+		(forward-sexp)
+		(buffer-substring-no-properties (region-beginning) (region-end))))
+
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
@@ -29,3 +45,5 @@
 (global-set-key (kbd "C-d") 'duplicate-line)
 (global-set-key (kbd "C-x ~") 'set-window-width-80)
 (global-set-key (kbd "C-M-l") 'clang-format-buffer)
+(global-set-key (kbd "C-z") 'undo)
+(global-set-key (kbd "C-x C-z") 'undo)
