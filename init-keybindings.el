fe9e1c65a2643ec44e8bb99105b2bf2b7d7a9ba5
Alexandru Pana <alexandru.pana@manabreakstudios.com>
2017-12-15 12:58:33 +0200

    Add support for clang-format

--- init-keybindings.el
+++ init-keybindings.el
@@ -1,8 +1,4 @@
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x C-b") 'helm-buffers-list)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "C-/") 'comment-line)
-
+(load "~/.emacs.d/modules/dev/clang-format/clang-format.el")
 
 (defun duplicate-line ()
   "Duplicate current line"
@@ -11,4 +7,25 @@
   (yank)
   (yank)
 	(previous-line))
+
+(defun set-window-width (n)
+  "Set the selected window's width."
+  (adjust-window-trailing-edge (selected-window) (- n (window-width)) t))
+
+(defun set-window-width-80 ()
+  "Set the selected window's width to 80 columns."
+	(interactive)
+	(set-window-width 80))
+
+(defun indent-buffer ()
+	(interactive)
+	(save-excursion
+		(indent-region (point-min) (point-max) nil)))
+
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-/") 'comment-line)
 (global-set-key (kbd "C-d") 'duplicate-line)
+(global-set-key (kbd "C-x ~") 'set-window-width-80)
+(global-set-key (kbd "C-M-l") 'clang-format-buffer)
