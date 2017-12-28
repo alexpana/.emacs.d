fa08d7d9ad16909bf829391fef6bbc80c857372f
Alexandru Pana <alexandru.pana@manabreakstudios.com>
2017-12-28 11:25:52 +0200

    Add generic ~/org/todo.org to org-mode agenda files

--- init.el
+++ init.el
@@ -58,6 +58,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (wombat)))
+ '(org-agenda-files (quote ("~/org/todo.org")))
  '(package-selected-packages
 	 (quote
 		(yasnippet-snippets yasnippet powerline helm magit company))))
